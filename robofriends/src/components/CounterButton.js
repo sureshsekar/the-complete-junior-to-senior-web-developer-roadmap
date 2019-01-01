35da65d6de7e2316dae322916d367c85ee4e936f
levijskal <levijskal@icloud.com>
2019-01-01 14:01:31 -0500

    test stateful

--- robofriends/src/components/CounterButton.js
+++ robofriends/src/components/CounterButton.js
@@ -19,7 +19,7 @@ import React, { PureComponent } from 'react';
  	}
 	render() {
 return (
-	<button color={this.props.color} onClick={this.updateCount}>
+	<button id="counter" color={this.props.color} onClick={this.updateCount}>
 	Count: {this.state.count}
 	</button>
 	)
